356de3d1f58d65611315b30961f3a67dc14ca9ba
ntomming <nikolaj-aare@hotmail.com>
2026-09-21 09:20:32 +0200

    Refactor message sending in monitor.py

--- monitor.py
+++ monitor.py
@@ -1,45 +1,23 @@
 import os
-2
 import requests
-3
- 
-4
+
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-5
 CHAT_ID = os.environ["CHAT_ID"]
-6
- 
-7
+
 message = """
-8
-✅ Festool/Makita Monitor virker!
-9
- 
-10
+Festool/Makita Monitor virker!
+
 Denne besked er sendt automatisk fra GitHub.
-11
 """
-12
- 
-13
+
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-14
- 
-15
+
 response = requests.post(
-16
-url,
-17
-data={
-18
-"chat_id": CHAT_ID,
-19
-"text": message
-20
-}
-21
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
 )
-22
- 
-23
+
 print(response.text)
