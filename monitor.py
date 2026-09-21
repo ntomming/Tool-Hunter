dcade8d8973cb52136dcc5ed42da5dd241666adc
ntomming <nikolaj-aare@hotmail.com>
2026-09-21 09:27:23 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,20 +4,35 @@ import requests
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-message = """
-Festool/Makita Monitor virker!
+SEARCH_TERMS = [
+    "Festool",
+    "Makita"
+]
 
-Denne besked er sendt automatisk fra GitHub.
+
+def send_telegram(text):
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": text
+        },
+        timeout=30
+    )
+
+
+for search_term in SEARCH_TERMS:
+
+    message = f"""
+🔎 Prisjagt søgning
+
+Mærke:
+{search_term}
+
+Link:
+https://www.prisjagt.dk/search?search={search_term}
 """
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    send_telegram(message)
 
-response = requests.post(
-    url,
-    data={
-        "chat_id": CHAT_ID,
-        "text": message
-    }
-)
-
-print(response.text)
+print("Done")
